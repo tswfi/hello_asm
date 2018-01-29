e6245892af098049616c096624df267843103d9a
Tatu Wikman <tatu.wikman@gmail.com>
2018-01-29 18:46:02 +0200

    trying to keep the cursor position in place

--- hello.asm
+++ hello.asm
@@ -4,7 +4,15 @@
 ; syscall listing can be found here http://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
 ;
 
+%define SYS_WRITE 1
+
 section .data
+    ; escape sequence for saving cursor position
+    savecursor db 1Bh, '[s'
+    .len equ $ - savecursor
+    ; escape sequence for returning cursor to the position saved
+    setcursor db 1Bh, '[10;20H'
+    .len equ $ - setcursor
 
 section .bss
     c: resb 1               ; buffer for our character to print
@@ -15,11 +23,78 @@ section .text
 ; output eax to stdout
 outchar:
     mov [c], eax            ; set the value into our buffer
-    mov rax, 1              ; sys_write
+    mov rax, SYS_WRITE      ; sys_write
     mov rdi, 1              ; stdout
-    mov rdx, 1              ; one character
+    mov rdx, 1               ; one character
     mov rsi, c              ; our buffer
     syscall                 ; call it
+    ret
+
+; output the full line (bit 8)
+outline:
+    ; hours
+    mov eax, 32             ; space character
+    call outchar
+
+    mov eax, 42             ; * character
+    call outchar
+
+    mov eax, 32             ; space character
+    call outchar
+
+    mov eax, 42             ; * character
+    call outchar
+
+    ; minutes
+    mov eax, 32             ; space character
+    call outchar
+    mov eax, 32             ; space character
+    call outchar
+
+    mov eax, 42             ; * character
+    call outchar
+
+    mov eax, 32             ; space character
+    call outchar
+
+    mov eax, 42             ; * character
+    call outchar
+
+    ; seconds
+    mov eax, 32             ; space character
+    call outchar
+    mov eax, 32             ; space character
+    call outchar
+
+    mov eax, 42             ; * character
+    call outchar
+
+    mov eax, 32             ; space character
+    call outchar
+
+    mov eax, 42             ; * character
+    call outchar
+
+    ; and a newline
+    mov eax, 10             ; newline
+    call outchar
+
+    ret
+
+
+outtime:
+    ; bit 8
+    call outline
+
+    ; bit 4
+    call outline
+
+    ; bit 2
+    call outline
+
+    ; bit 1
+    call outline
+
     ret
 
 ; quit with zero return code
@@ -28,18 +103,38 @@ quit_zero:
     mov    rdi, 0           ; code
     syscall
 
+savecursorposition:
+    ; save our cursor position
+    mov     rax, 1
+    mov     rdi, 1
+    mov     rsi, savecursor
+    mov     rdx, savecursor.len
+    syscall                 ; call it
+
+    ret
+
+returncursorposition:
+    ; return the cursor back to the saved position
+    mov     rax, 1
+    mov     rdi, 1
+    mov     rsi, setcursor
+    mov     rdx, setcursor.len
+    syscall                 ; call it
+
+    ret
+
 ; main
 _start:
-    mov eax, 42             ; * character
-    call outchar
+    nop                     ; break here :)
 
-    mov eax, 10             ; newline
-    call outchar
+    inc rcx
 
-    mov eax, 42             ; * character
-    call outchar
+    call savecursorposition
 
-    mov eax, 10             ; newline
-    call outchar
+    call outtime
+
+    call returncursorposition
+
+    call outtime
 
     jmp quit_zero
