4b22c34a73f511935af355df4322448edc5c1c14
Tatu Wikman <tatu.wikman@gmail.com>
2018-01-29 16:55:15 +0200

    print a character

--- hello.asm
+++ hello.asm
@@ -1,14 +1,45 @@
+;
+; Someday this will be a Simple clock written in assembler
+;
+; syscall listing can be found here http://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
+;
+
 section .data
-    msg db      "hello, world!"
+
+section .bss
+    c: resb 1               ; buffer for our character to print
 
 section .text
     global _start
+
+; output eax to stdout
+outchar:
+    mov [c], eax            ; set the value into our buffer
+    mov rax, 1              ; sys_write
+    mov rdi, 1              ; stdout
+    mov rdx, 1              ; one character
+    mov rsi, c              ; our buffer
+    syscall                 ; call it
+    ret
+
+; quit with zero return code
+quit_zero:
+    mov    rax, 60          ; sys_exit
+    mov    rdi, 0           ; code
+    syscall
+
+; main
 _start:
-    mov     rax, 1
-    mov     rdi, 1
-    mov     rsi, msg
-    mov     rdx, 13
-    syscall
-    mov    rax, 60
-    mov    rdi, 0
-    syscall
+    mov eax, 42             ; * character
+    call outchar
+
+    mov eax, 10             ; newline
+    call outchar
+
+    mov eax, 42             ; * character
+    call outchar
+
+    mov eax, 10             ; newline
+    call outchar
+
+    jmp quit_zero
